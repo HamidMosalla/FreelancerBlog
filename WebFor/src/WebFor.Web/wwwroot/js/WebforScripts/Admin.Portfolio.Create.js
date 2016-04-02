37aba0f1088b2ce4989d39e894aeea25aa584c34
Hamid Mosalla <xellarix@live.com>
2016-04-02 22:19:07 +0430

    adding controller and create view for portfolio, it's up to a good start now.

---
+++ WebFor/src/WebFor.Web/wwwroot/js/WebforScripts/Admin.Portfolio.Create.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    $.validator.setDefaults({ ignore: '' });
+    $(function() {
+        "use strict";
+
+        $("#PortfolioForm").validate({
+            ignore: []
+        });
+
+        var editor = CKEDITOR.replace('PortfolioBody');
+        
+        $('#PortfolioDateBuilt').pikaday({ firstDay: 1, format: 'MM/DD/YYYY' });
+
+    });
+})();
