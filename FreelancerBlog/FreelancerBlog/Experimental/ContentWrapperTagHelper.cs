b6a181b61eb99a0fb31034b05bbc0886e49f7c22
Hamid Mosalla <Xellarix@gmail.com>
2018-01-19 22:24:43 +0330

    add new tag helpers

---
+++ FreelancerBlog/FreelancerBlog/Experimental/ContentWrapperTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FreelancerBlog.Experimental
+{
+    public class ContentWrapperTagHelper : TagHelper
+    {
+        public bool IncludeHeader { get; set; } = true;
+        public bool IncludeFooter { get; set; } = true;
+        public string Title { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", "panel-body");
+            TagBuilder title = new TagBuilder("h1");
+            title.InnerHtml.Append(Title);
+            TagBuilder container = new TagBuilder("div");
+            container.Attributes["class"] = "bg-info panel-body";
+            container.InnerHtml.AppendHtml(title);
+
+            if (IncludeHeader) output.PreElement.SetHtmlContent(container);
+
+            if (IncludeFooter) output.PostElement.SetHtmlContent(container);
+        }
+    }
+}
---
+++ FreelancerBlog/FreelancerBlog/Experimental/FormButtonTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FreelancerBlog.Experimental
+{
+    [HtmlTargetElement("formbutton")]
+    public class FormButtonTagHelper : TagHelper
+    {
+        public string Type { get; set; } = "Submit";
+        public string BgColor { get; set; } = "primary";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", $"btn btn-{BgColor}");
+            output.Attributes.SetAttribute("type", Type);
+            output.Content.SetContent(Type == "submit" ? "Add" : "Reset");
+        }
+    }
+}
