7a82f9217f10a02f631da132bfc4b3538dbb7fcc
Hamid Mosalla <Xellarix@gmail.com>
2018-01-19 19:01:38 +0330

    add button tag helper

---
+++ FreelancerBlog/FreelancerBlog/Experimental/ButtonTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FreelancerBlog.Experimental
+{
+    // <button bs-button-color="danger">asdfsadfsdf</button>
+
+    //html element sensitive to the starting name of the class
+    [HtmlTargetElement("button", Attributes = "bs-button-color", ParentTag = "form")]
+    [HtmlTargetElement("a", Attributes = "bs-button-color", ParentTag = "form")]
+    public class ButtonTagHelper : TagHelper
+    {
+        //attribute name sensitive to property name
+        public string BsButtonColor { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"btn btn-{BsButtonColor}");
+        }
+    }
+}
