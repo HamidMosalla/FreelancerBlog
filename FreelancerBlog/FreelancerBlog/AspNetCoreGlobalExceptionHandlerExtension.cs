75ef9b83d870d4b692cbf46751306e9ec95f0857
Hamid Mosalla <Xellarix@gmail.com>
2018-02-15 10:23:37 +0330

    add serilog

---
+++ FreelancerBlog/FreelancerBlog/AspNetCoreGlobalExceptionHandlerExtension.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+
+namespace FreelancerBlog
+{
+    public static class AspNetCoreGlobalExceptionHandlerExtension
+    {
+        public static IApplicationBuilder UseAspNetCoreExceptionHandler(this IApplicationBuilder app)
+        {
+            var loggerFactory = app.ApplicationServices.GetService(typeof(ILoggerFactory)) as ILoggerFactory;
+
+            return app.UseExceptionHandler(HandleException(loggerFactory));
+        }
+
+        public static Action<IApplicationBuilder> HandleException(ILoggerFactory loggerFactory)
+        {
+            return appBuilder =>
+            {
+                appBuilder.Run(async context =>
+                {
+                    var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (exceptionHandlerFeature != null)
+                    {
+                        var logger = loggerFactory.CreateLogger("Serilog Global exception logger");
+                        logger.LogError(500, exceptionHandlerFeature.Error, exceptionHandlerFeature.Error.Message);
+                    }
+
+                    context.Response.Redirect($"/Error/Status/{context.Response.StatusCode}");
+                });
+            };
+        }
+    }
+}
