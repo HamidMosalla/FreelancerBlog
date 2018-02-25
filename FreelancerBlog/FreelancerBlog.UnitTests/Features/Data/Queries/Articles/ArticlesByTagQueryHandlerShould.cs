1d369469054d4613fa1a65b9e1ec1a98c50b8af5
Hamid Mosalla <Xellarix@gmail.com>
2018-02-25 21:50:48 +0330

    add unit test for ArticlesByTagQueryHandler

--- FreelancerBlog/FreelancerBlog.UnitTests/Features/Data/Queries/Articles/ArticlesByTagQueryHandlerShould.cs
+++ FreelancerBlog/FreelancerBlog.UnitTests/Features/Data/Queries/Articles/ArticlesByTagQueryHandlerShould.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using FreelancerBlog.Core.DomainModels;
+using FreelancerBlog.Core.Queries.Data.Articles;
 using FreelancerBlog.Data.Queries.Articles;
 using FreelancerBlog.UnitTests.Database;
 using Xunit;
@@ -11,17 +17,41 @@ namespace FreelancerBlog.UnitTests.Features.Data.Queries.Articles
     {
         private ArticlesByTagQueryHandler _sut;
 
+        public ArticlesByTagQueryHandlerShould()
+        {
+            _sut = new ArticlesByTagQueryHandler(Context);
+        }
+
         protected override void LoadTestData()
         {
-            base.LoadTestData();
+            var articles = new List<Article>
+            {
+                new Article {ArticleId = 1, ArticleTitle = "A"},
+                new Article {ArticleId = 2, ArticleTitle = "B"}
+            };
+
+            var articleTags = new List<ArticleTag> { new ArticleTag { ArticleTagId = 1, ArticleTagName = "H" } };
+
+            var articleArticleTag = new List<ArticleArticleTag>
+            {
+                new ArticleArticleTag {ArticleId = 1, ArticleTagId = 1},
+                new ArticleArticleTag {ArticleId = 2, ArticleTagId = 2}
+            };
+
+            Context.Articles.AddRange(articles);
+            Context.ArticleTags.AddRange(articleTags);
+            Context.ArticleArticleTags.AddRange(articleArticleTag);
+            Context.SaveChanges();
         }
 
         [Fact]
-        public void Always_ReturnTheCorrectType()
+        public async Task Always_ReturnTheCorrectType()
         {
-            
+            var message = new ArticlesByTagQuery { TagId = 1 };
 
+            var result = await _sut.Handle(message, default(CancellationToken));
 
+            result.First().Should().BeOfType<Article>();
         }
     }
 }
