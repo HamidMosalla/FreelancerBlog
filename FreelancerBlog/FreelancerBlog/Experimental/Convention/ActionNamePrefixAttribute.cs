6f94ed725ce97ae525f91b013cdfd475111e86a9
Hamid Mosalla <Xellarix@gmail.com>
2018-02-26 22:54:52 +0330

    add model convention sample and action constraint

---
+++ FreelancerBlog/FreelancerBlog/Experimental/Convention/ActionNamePrefixAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace FreelancerBlog.Experimental.Convention
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class ActionNamePrefixAttribute : Attribute, IActionModelConvention
+    {
+        private string namePrefix;
+        public ActionNamePrefixAttribute(string prefix)
+        {
+            namePrefix = prefix;
+        }
+        public void Apply(ActionModel action)
+        {
+            action.ActionName = namePrefix + action.ActionName;
+        }
+    }
+}
---
+++ FreelancerBlog/FreelancerBlog/Experimental/Convention/UserAgentAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace FreelancerBlog.Experimental.Convention
+{
+    public class UserAgentAttribute : Attribute, IActionConstraint
+    {
+        private string substring;
+        public UserAgentAttribute(string sub)
+        {
+            substring = sub.ToLower();
+        }
+        public int Order { get; set; } = 0;
+        public bool Accept(ActionConstraintContext context)
+        {
+            return context.RouteContext.HttpContext
+                .Request.Headers["User-Agent"]
+                .Any(h => h.ToLower().Contains(substring));
+        }
+    }
+}
