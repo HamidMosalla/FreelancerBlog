844b73a67a64e2d1317f6d279b997af3d7bcb7fc
Hamid Mosalla <Xellarix@gmail.com>
2019-08-27 08:23:02 +0200

    Update nuget package

---
+++ FreelancerBlog/FreelancerBlog.Data/Commands/Wrappers/IPathWrapper.cs
@@ -0,0 +1,9 @@
+﻿namespace FreelancerBlog.Core.Wrappers
+{
+    public interface IPathWrapper
+    {
+        string GetFileNameWithoutExtension(string path);
+        string GetExtension(string path);
+        string Combine(params string[] paths);
+    }
+}
